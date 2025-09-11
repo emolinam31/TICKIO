e2450f2ab64fb2c9d2935134b88c7fe0ffe0deb1
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-09-11 09:44:22 -0500

    Primera Version del template About

--- Tickio_project/tickio/urls.py
+++ Tickio_project/tickio/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),   
+    path("admin/", admin.site.urls),
+    path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
 ]
